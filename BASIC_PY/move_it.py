a3b20d22c32afdbf8f3cca1abedd9c1a675607a7
luxasscze <lukas2slivka@gmail.com>
2023-11-23 16:12:58 +0000

    wait

--- BASIC_PY/move_it.py
+++ BASIC_PY/move_it.py
@@ -43,7 +43,7 @@ def spin_right(duty, tm):
 def program_one():
     rnd_duty = random.uniform(18, 35)
     rnd_time = random.uniform(0.1, 1)
-    rnd_wait_end = random.uniform(0.1, 1)
+    rnd_wait_end = 0.1
     print("move forward")
     move_forward(rnd_duty, random.uniform(0.1, 1.5))
     time.sleep(random.uniform(0.1, rnd_wait_end))
