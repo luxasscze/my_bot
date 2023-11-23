53910cee6bc1326241ca1d99eeb098934eab2d86
luxasscze <lukas2slivka@gmail.com>
2023-11-23 09:11:04 +0000

    changed default PY

--- BASIC_PY/gpio_test.py
+++ BASIC_PY/gpio_test.py
@@ -12,18 +12,24 @@ s = GPIO.PWM(32, 90)
 p.start(0)
 s.start(0)
 
+print("starting motor test")
+
 try:
     while 1:
         for dc in range(0, 101, 5):
+            print("phase A")
             s.ChangeDutyCycle(dc)
             time.sleep(1)
         for dc in range(100, -1, -5):
+            print("phase B")
             s.ChangeDutyCycle(dc)
             time.sleep(1)
         for dc in range(0, 101, 5):
+            print("phase C")
             p.ChangeDutyCycle(dc)
             time.sleep(1)
         for dc in range(100, -1, -5):
+            print("phase D")
             p.ChangeDutyCycle(dc)
             time.sleep(1)
 except KeyboardInterrupt:
